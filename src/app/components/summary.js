78be28197eced19a5b60517e195652aeca1d8fec
Nishit-Sharma <nishit.sharma@mcvts.org>
2024-05-29 22:39:21 -0400

    Fixing more things!!

--- src/app/components/summary.js
+++ src/app/components/summary.js
@@ -1,13 +1,27 @@
 import Image from "next/image";
+import Picture from "../static/NishitSharma.jpg";
+
+const styleImage = {
+    borderRadius: "10%",
+    width: "300px",
+    height: "300px",
+    overflow: "hidden",
+    objectFit: "cover",
+    objectPosition: "center",
+    margin: "0 auto",
+};
 
 export default function summary() {
     return (
         <main>
             <div className="container mx-auto px-4 center-true flex flex-row">
-                <div className="container mx-auto px-4 center-true bg-gray-500 text-center">
+                <div className="container mx-auto py-4 px-4 center-true bg-gray-500 text-center hover:object-top rounded">
+                    <div className="rounded-xl w-80 h-80 overflow-hidden object-cover object-center mx-auto">
+                        <Image src={Picture} alt="Nishit Sharma"/>
+                    </div>
                     <h1>Nishit Sharma</h1>
                 </div>
-                <div className="container mx-auto px-4 center-true bg-red-500 text-center">
+                <div className="container mx-auto px-4 center-true bg-red-500 text-center rounded">
                     <h1>Other Side</h1>
                 </div>
             </div>
