5838f03d8457318776afd7cb5d33f7d30d6313c7
Nishit-Sharma <nishit.sharma@mcvts.org>
2024-05-29 22:53:20 -0400

    Final changes for today

--- src/app/components/summary.js
+++ src/app/components/summary.js
@@ -13,13 +13,14 @@ const styleImage = {
 
 export default function summary() {
     return (
+        // COLORS ARE TEMP MAKE SURE TO CHANGE THEM
         <main>
             <div className="container mx-auto px-4 center-true flex flex-row">
                 <div className="container mx-auto py-4 px-4 center-true bg-gray-500 text-center hover:object-top rounded">
                     <div className="rounded-xl w-80 h-80 overflow-hidden object-cover object-center mx-auto">
                         <Image src={Picture} alt="Nishit Sharma"/>
                     </div>
-                    <h1>Nishit Sharma</h1>
+                    <h1>Hello! My name is Nishit Sharma</h1>
                 </div>
                 <div className="container mx-auto px-4 center-true bg-red-500 text-center rounded">
                     <h1>Other Side</h1>
