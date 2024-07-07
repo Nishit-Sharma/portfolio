3aae0491923eaa49b9642732c7032a27a9462c81
Nishit-Sharma <nishit.sharma@mcvts.org>
2024-07-07 16:01:29 -0400

    fix?

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import {
   motion,
   useScroll,
   useTransform,
   AnimatePresence,
 } from "framer-motion";
-
-const { window } = globalThis;
 
 import Picture from "./static/NishitSharma.JPG";
 import ResumeIcon from "./static/resume.png";
@@ -31,9 +29,11 @@ export default function Home() {
 
   const infoRef = useRef(null);
 
-  function isMobile() {
-    return window.innerWidth < 640;
-  }
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth < 640);
+  }, []);
 
   function handleResumeClick() {
     if (transcriptClick) {
@@ -109,7 +109,7 @@ export default function Home() {
   }
 
   function NameAndPicture() {
-    return isMobile() ? (
+    return isMobile ? (
       <div className="container flex flex-col items-center content-center justify-center px-4 py-6 bg-black-500">
         <div className="container w-auto px-10 py-6 mx-5 my-5 text-2xl text-center duration-500 shadow-md opacity-100 rounded-3xl bg-black-600 hover:scale-105">
           <h1>Hello, My name is Nishit Sharma!</h1>
@@ -141,7 +141,7 @@ export default function Home() {
   }
 
   function Information() {
-    return isMobile() ? (
+    return isMobile ? (
       <div className="container flex flex-col items-center content-center justify-center px-4 py-6 bg-black-500">
         {resumeClick ? (
           <Resume />
@@ -220,54 +220,145 @@ export default function Home() {
       setSelectedProject(null);
     };
 
-    return (
-      isMobile() ? (
-        <div>
-          <div className="container flex flex-row items-center content-center justify-center w-auto py-6 pb-10 mx-auto text-center">
-            <div className="container px-10 py-6 mx-5 text-center duration-500 shadow-md rounded-3xl bg-black-600 hover:scale-105">
-              <h1 className="text-xl">A.L.P.H.A Personal Assistant</h1>
-              <p>
-                <br></br>A personal assistant designed to make life easier.
-                Designed with React and Python
-              </p>
-              <button
-                onClick={() => handleProjectClick("assistant")}
-                className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-              >
-                View Details
-              </button>
-              
-            </div>
-            <div className="container px-10 py-6 mx-5 text-center duration-500 shadow-md rounded-3xl bg-black-600 hover:scale-105">
-              <h1 className="text-xl">FRC Charged Up Robot</h1>
-              <p>
-                <br></br>
-                The code for MCST's FRC Robot during the Charged Up Year, 2023
-              </p>
-              <button
-                onClick={() => handleProjectClick("robotics")}
-                className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-              >
-                View Details
-              </button>
-            </div>
-            <div className="container px-10 py-6 mx-5 text-center duration-500 shadow-md rounded-3xl bg-black-600 hover:scale-105">
-              <h1 className="text-xl">My Portfolio</h1>
-              <p>
-                <br></br>
-                This Portfolio website!
-              </p>
-              <button
-                onClick={() => handleProjectClick("web-development")}
-                className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-              >
-                View Details
-              </button>
-            </div>
+    return isMobile ? (
+      <div>
+        <div className="container flex flex-row items-center content-center justify-center w-auto py-6 pb-10 mx-auto text-center">
+          <div className="container px-10 py-6 mx-5 text-center duration-500 shadow-md rounded-3xl bg-black-600 hover:scale-105">
+            <h1 className="text-xl">A.L.P.H.A Personal Assistant</h1>
+            <p>
+              <br></br>A personal assistant designed to make life easier.
+              Designed with React and Python
+            </p>
+            <button
+              onClick={() => handleProjectClick("assistant")}
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+            >
+              View Details
+            </button>
+          </div>
+          <div className="container px-10 py-6 mx-5 text-center duration-500 shadow-md rounded-3xl bg-black-600 hover:scale-105">
+            <h1 className="text-xl">FRC Charged Up Robot</h1>
+            <p>
+              <br></br>
+              The code for MCST's FRC Robot during the Charged Up Year, 2023
+            </p>
+            <button
+              onClick={() => handleProjectClick("robotics")}
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+            >
+              View Details
+            </button>
+          </div>
+          <div className="container px-10 py-6 mx-5 text-center duration-500 shadow-md rounded-3xl bg-black-600 hover:scale-105">
+            <h1 className="text-xl">My Portfolio</h1>
+            <p>
+              <br></br>
+              This Portfolio website!
+            </p>
+            <button
+              onClick={() => handleProjectClick("web-development")}
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+            >
+              View Details
+            </button>
           </div>
         </div>
-      ) : (
-<div>
+
+        {isModalOpen && (
+            <div
+              className="fixed top-0 left-0 flex items-center justify-center w-full h-screen p-10 bg-opacity-50 bg-black-500"
+              onClick={handleModalClose}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <div
+                animate={{ y: -20, opacity: 1 }}
+                initial={{ y: 0, opacity: 0 }}
+                exit={{ y: 20, opacity: 0 }}
+                className="p-10 text-center bg-white-500 text-black-500 rounded-3xl"
+              >
+                {selectedProject === "assistant" && (
+                  <div>
+                    <h1 className="text-xl">A.L.P.H.A Personal Assistant</h1>
+                    <p>
+                      <br></br>
+                      This was a collaborative project was submitted for the
+                      NJTSA Software Development competition. It is a personal
+                      assistant that was designed to make ordinary tasks easier.
+                      Telling the time, date, weather, googling things are all
+                      possible with A.L.P.H.A. It was designed with React and
+                      Python, where React was used for the front end and Python
+                      was used for the backend. I primarily worked on the
+                      connection between React and Python, and the Python file
+                      itself.
+                    </p>
+                    <a
+                      href="https://github.com/Nishit-Sharma/NJTSA-Software-Development-LocalHost"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                        View on GitHub
+                      </button>
+                    </a>
+                  </div>
+                )}
+                {selectedProject === "robotics" && (
+                  <div>
+                    <h1 className="text-xl">FRC Charged Up Robot</h1>
+                    <p>
+                      <br></br>
+                      This was also a collaborative project where I worked with
+                      my team to program our bot for the 2023 FRC season. We
+                      used Java to program the robot, and I primarily worked on
+                      the autonomous code and the drive code. This was the first
+                      big project I worked on as a team and I took a lot away
+                      from it.
+                    </p>
+                    <a
+                      href="https://github.com/mcstrobotics/ChargedUp8588"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                        View on GitHub
+                      </button>
+                    </a>
+                  </div>
+                )}
+                {selectedProject === "web-development" && (
+                  <div>
+                    <h1 className="text-xl">My Portfolio</h1>
+                    <p>
+                      <br></br>
+                      This portfolio website was my first time using Next.js,
+                      and I learned a lot about React and the thought process of
+                      a Web Designer. I used Tailwind CSS for styling and Framer
+                      Motion for animations, which were also two new frameworks
+                      for me. I also used Vercel for deployment to make sure
+                      that my website was easily accessible. I learned a lot
+                      about web development, React, and Next.js from this
+                      project and it changed my perspective on web development
+                      in a positive way.
+                    </p>
+                    <a
+                      href="https://github.com/Nishit-Sharma/portfolio"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                        View on GitHub
+                      </button>
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+      </div>
+    ) : (
+      <div>
         <motion.div
           style={{ opacity: buttonOpacity }}
           className="container flex flex-row items-center content-center justify-center w-auto py-6 pb-10 mx-auto text-center"
@@ -408,17 +499,14 @@ export default function Home() {
           )}
         </AnimatePresence>
       </div>
-      )
     );
   }
 
   function BeforeScroll() {
-    return (
-        isMobile() ? (
-          <>
-          </>
-        ) : (
-          <>
+    return isMobile ? (
+      <></>
+    ) : (
+      <>
         <motion.div
           style={{
             x: x,
@@ -448,7 +536,6 @@ export default function Home() {
           ></button>
         </motion.div>
       </>
-        )
     );
   }
 
