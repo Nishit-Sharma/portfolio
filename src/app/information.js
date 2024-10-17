3e5e01fcd954b032d5a88987a473303cc0e51b69
Nishit-Sharma <nishit.sharma@mcvts.org>
2024-10-17 00:44:09 -0400

    trying to make it only animate once

--- src/app/information.js
+++ src/app/information.js
@@ -13,6 +13,7 @@ function Summary() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{delay: 3}}
+      viewport={{once: true}}
       className="container px-10 py-6 mx-10 text-center duration-500 shadow-2xl rounded-3xl bg-black-600 hover:scale-105 w-96"
     >
       <h1>
