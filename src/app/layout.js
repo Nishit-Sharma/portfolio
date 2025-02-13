251e0ce8d1f1a68c8b46e352ff67b590e4090e51
Nishit-Sharma <nishit.sharma@mcvts.org>
2025-02-12 21:53:24 -0500

    Added Google Search Console Metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="meaEsdJTzch5FVrbbHVC9e4UxjYXhddevXhmF_XJypI" />
       <body>
         <ClientLayout>{children}</ClientLayout>
         <Analytics />
